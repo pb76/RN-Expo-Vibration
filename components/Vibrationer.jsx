feb554422bf20428083495960008d41df7539ab4
Patrik Bernhardsson <pb76@users.noreply.github.com>
2025-10-02 13:27:26 +0200

    Lagt till komponenten Vibrationer och uppdaterade App.jsx för att inkludera den

---
+++ components/Vibrationer.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Vibrationer() {
+  return (
+    <View style={styles.container}>
+      <Text>Här kommer vi arbeta med vibrationer</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
