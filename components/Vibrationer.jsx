27e827ea2ecf579e60df66a640ca8384a49b7221
Patrik Bernhardsson <pb76@users.noreply.github.com>
2025-10-02 13:31:55 +0200

    Lagt till vibration funktionalitet i Vibrationer-komponenten

--- components/Vibrationer.jsx
+++ components/Vibrationer.jsx
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, Platform, StyleSheet, Text, Vibration, View } from "react-native";
+// https://reactnative.dev/docs/vibration
 
 export default function Vibrationer() {
+  const ONE_SECOND_IN_MS = 1000;
+
+  const PATTERN = [1 * ONE_SECOND_IN_MS, 2 * ONE_SECOND_IN_MS, 3 * ONE_SECOND_IN_MS];
+
   return (
     <View style={styles.container}>
       <Text>Här kommer vi arbeta med vibrationer</Text>
+      <View>
+        <Button title="Vibrera en gång" onPress={() => Vibration.vibrate()} />
+        <Button title="Vibrera med mönster" onPress={() => Vibration.vibrate(PATTERN)} />
+      </View>
     </View>
   );
 }
